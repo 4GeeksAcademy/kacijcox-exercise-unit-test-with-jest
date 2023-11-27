e70ac3e2eb7207130bca7d8eef55d8107a31bd83
kacijcox <145575906+kacijcox@users.noreply.github.com>
2023-11-27 23:15:13 +0000

    Second commit

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-const fromEuroToDollar = function(valueInEuro) {
-    // Convert the given valueInEuro to dollars
-    let valueInDollar = valueInEuro * 1.07;
-    // return the dollar value
-    return valueInDollar;
-}
+const oneEuroIs = {
+    JPY: 129.5, 
+    USD: 1.07,  
+};
 
 const fromDollarToYen = function(valueInDollar) {
     let valueInYen = valueInDollar * oneEuroIs.JPY / oneEuroIs.USD;
     return valueInYen;
 }
 
-const fromYenToPound = function(valueInYen) {
-    let valueInPound = valueInYen * oneEuroIs.GBP / oneEuroIs.JPY;
-    return valueInPound;
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
+    return valueInDollar;
 }
 
-const sum =(a,b) => {
-    return a + b
+
+const sum = function(a, b) {
+    return a + b;
 }
 
-console.log(sum(7,3))
-
-module.exports = { sum, fromEuroToDollar }
-
-console.log(fromEuroToDollar(10));     
-console.log(fromDollarToYen(10));      
-console.log(fromYenToPound(1000));    
+module.exports = { fromDollarToYen, fromEuroToDollar, sum };
--- test.js
+++ test.js
@@ -1,19 +1,18 @@
-const { sum } = require('./app.js');
-test ('adds 14 + 9 to equal 23', () => {
-    let total = sum(14,9);
+const { sum, fromEuroToDollar, fromDollarToYen } = require('./app.js');
+
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
     expect(total).toBe(23);
 });
 
-test("One euro should be 1.07 dollars", function() {
-    // Import the function from app.js
-    const { fromEuroToDollar } = require('./app.js');
-
-    // Use the function like its supposed to be used
+test("One euro should be 1.07 dollars", function () {
     const dollars = fromEuroToDollar(3.5);
+    const expected = 3.5 * 1.07;
+    expect(dollars).toBe(expected);
+});
 
-    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
-    const expected = 3.5 * 1.07; 
-    
-    // This is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
-})
+test("Convert 10 dollars to yen", function () {
+    const yen = fromDollarToYen(10);
+    const expected = 10 * 129.5 / 1.07; // Using the conversion rates
+    expect(yen).toBe(expected);
+});
